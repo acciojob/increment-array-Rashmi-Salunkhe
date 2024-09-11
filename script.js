bfba53d1943a9df0352b1cd35a22deeb6577eb0f
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-09-11 20:44:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let arr = [1, 2, 3, 4, 5];
+
 function incrementArray(arr) {
-  //your code here
+  // Create a new array by mapping over the input array and incrementing each element
+  return arr.map(num => ++num); // using the increment operator
 }
 
-alert(incrementArray(arr));
+alert(incrementArray(arr)); // Output: [2, 3, 4, 5, 6]
